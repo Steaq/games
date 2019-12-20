a4b078a855fe8180598e1f79bc42025cb9fa86f4
m <m@internetz>
2019-12-20 18:04:22 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: ['static/js/games.js'],
+        dest: 'static/js/games.min.js'
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'static/css/style.min.css': ['static/css/style.css']
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+};
+
