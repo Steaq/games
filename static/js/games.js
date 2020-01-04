632f0fbefa28fe10ad76fa6cef66cf8bc7aa0e3c
m <m@internetz>
2020-01-04 16:26:53 +0200

    search update

--- static/js/games.js
+++ static/js/games.js
@@ -21,4 +21,42 @@ $(document).ready(function () {
 		return false;
 	});
 
+    // Sonstructs the suggestion engine
+    /*var games = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // The url points to a json file that contains an array of country names
+        prefetch: 'http://api.gamesportal.com/search.json'
+    });*/
+    /*
+    $('.typeahead').typeahead(null, {
+        name: 'games',
+        source: games,
+    });*/
+
+	var searchGames = new Bloodhound({
+	  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+	  queryTokenizer: Bloodhound.tokenizers.whitespace,
+	  prefetch: 'http://api.gamesportal.com/search.json',
+	  remote: {
+	    url: 'http://api.gamesportal.com/search.json?q=%QUERY',
+	    wildcard: '%QUERY'
+	  }
+	});
+
+	/*$('#remote .typeahead').typeahead(null, {
+	  name: 'best-pictures',
+	  display: 'value',
+	  source: bestPictures
+	});*/
+
+	$('#custom-templates .typeahead').typeahead(null, {
+	  name: 'searchGames',
+	  display: 'name',
+	  source: searchGames,
+	  templates: {
+	    suggestion: Handlebars.compile('<div><strong>{{name}}</strong> – {{description}}</div>')
+	  }
+	});
+
 });
